5719e607a22d32884d1f34c9f7681e4953eea40e
mdabski <dabrowski.mark@gmail.com>
2015-05-16 14:02:38 +0000

    saving point for linking photos and observations

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.2.0'
+ruby '2.1.5'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
@@ -14,6 +14,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+
+gem "paperclip", "~> 4.2"
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,13 +15,13 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-   config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'localhost:3000',
-  user_name:            'riverrangersca@gmail.com',
+  user_name:            Rails.application.secrets.email,
   password:             Rails.application.secrets.email_pass,
   authentication:       'plain',
   enable_starttls_auto: true  }
