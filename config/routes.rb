d683b0cbc8cedb1117d0717b1a413231a7d76d88
Ollie Kavanagh <olliek@gmail.com>
2015-05-18 13:12:14 -0400

    Restructure the map view and get observations showing

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
   get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 
-  root :to => 'map#index'
+  root :to => 'observations#index'
 
   resources :password_resets
 
