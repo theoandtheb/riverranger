3d317fcab62e54057291688250d42971e0af9b53
Ollie Kavanagh <olliek@gmail.com>
2015-05-19 12:34:30 -0400

    Show the twitter feed on the news page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,12 @@
 module ApplicationHelper
 
+  include Twitter::Autolink
+ 
+  def twitter_text(text)
+    text = auto_link(text)
+    text ? text.html_safe : ''
+  end
+
   def body_class
     return "#{controller.controller_name}-#{controller.action_name} #{controller.controller_name} #{controller.action_name} #{params[:permalink]}"
   end
