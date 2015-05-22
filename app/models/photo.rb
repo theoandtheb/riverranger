a967842f2f1011a07273b44f6e92fcdab347a596
mdabski <dabrowski.mark@gmail.com>
2015-05-22 11:53:02 +0000

    small fixes to photos for observations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   belongs_to :observation
   
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :default_size => "800x450", :thumb => "100x100>" }, :default_url => ActionController::Base.helpers.asset_path('default_observation_image.jpg')
   
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
