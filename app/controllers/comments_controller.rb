f7be254b3ead5df0ee1af00ad8b32753f664d8bb
Ollie Kavanagh <olliek@gmail.com>
2015-05-28 12:07:46 -0400

    Fix observation date on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -71,6 +71,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:body, :observation_id, :user_id)
+      params.require(:comment).permit(:body, :observe_on, :observation_id, :user_id)
     end
 end
