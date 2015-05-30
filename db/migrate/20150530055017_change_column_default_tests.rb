0d7d203f58320046320b9e5d7c11416119710133
theoandtheb <blue.apple@sympatico.ca>
2015-05-30 03:26:47 -0400

    Removed unnecessary migrations.

--- db/migrate/20150530055017_change_column_default_tests.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnDefaultTests < ActiveRecord::Migration
-  def change
-  	change_column_default(:tests, :observe_on, Time.now)
-  end
-end
--- db/migrate/20150530055534_change_column_default_tests_again.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnDefaultTestsAgain < ActiveRecord::Migration
-  def change 
-  	change_column_default(:tests, :observe_on, nil)
-  end
-end
